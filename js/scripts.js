5e034ae4a7f70c88b56234aa4efec1cac8d23913
Kamlton <kamlton13@gmail.com>
2021-04-03 17:05:06 +0300

    verstaem

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+var userName = "Kamil";
+console.log(userName);
+
+
+var integer = 16;
+ console.log(integer);
+
+ var description = 'не женат,детей не имею';
+console.log(description);
+
+
+
+
+var a = 23;
+var b = 10;
+var result = a * b; 
+console.log( result);
+
+
+
+var a = 10;
+var b = 4;
+var result = a * b; 
+console.log( result);
+
+
+//
